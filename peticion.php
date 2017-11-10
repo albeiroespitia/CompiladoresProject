fb90b82228995b81578aae0776edfbbf80230714
JGUETO <joseeguerreroot@gmail.com>
2017-11-10 03:40:55 -0600

    Se añade archivo pruebas para depurar
    El proceso de automatización va en un 80%, no se agrego librerias. Falta agregarle un diseño a las palabras que se muestran, hacer las peticiones con la voz y al finalizar la traduccion empieze desde la ultima seña.

--- peticion.php
+++ peticion.php
@@ -1,53 +1,43 @@
 <?php
-
 	function connectDB(){
-
 	   $conexion = mysqli_connect("localhost", "root", "", "enseniasbd");
 	 
 	    return $conexion;
 	}
 	function disconnectDB($conexion){
-
     $close = mysqli_close($conexion);
-
     return $close;
 	}
-
-
 	function getArraySQL($sql){
     //Creamos la conexión con la función anterior
     $conexion = connectDB();
 
     //generamos la consulta
-
     mysqli_set_charset($conexion, "utf8"); //formato de datos utf8
     
     if(!$result = mysqli_query($conexion, $sql)) die(); //si la conexión cancelar programa
-
     $rawdata = array(); //creamos un array
-
     //guardamos en un array multidimensional todos los datos de la consulta
     $i=0;
-
     while($row = mysqli_fetch_array($result))
     {
         $rawdata[$i] = $row;
         $i++;
     }
      disconnectDB($conexion); //desconectamos la base de datos
-
     return $rawdata; //devolvemos el array
 }
 	$txt = $_POST['txt']; // Variable post
 	//$txt = $_GET['txt'];
 	$myArray = [];
+	//echo $txt;
+	
 	foreach ($txt as &$imagen) {
 	$sql = 'SELECT * FROM imagen WHERE nombre = "'.$imagen.'" ';
 	
 	$myArray =  array_merge($myArray, getArraySQL($sql));
     
 	}
-
 	/*
 	if(empty($myArray)){
 	
@@ -56,7 +46,6 @@
 		foreach ($txt as &$text2) {
 		$ArrayTexto = str_split( $text2);
 		}
-
 		$myArray = [];	
 		foreach ($ArrayTexto as &$imagen) {
 			$sql = 'SELECT * FROM imagen WHERE nombre = "'.$imagen.'" ';
@@ -64,7 +53,6 @@
 			$myArray =  array_merge($myArray, getArraySQL($sql));
 		    
 		}
-
 	}
 	*/
 	echo json_encode($myArray);	
